a5a50245aabe7bca9cc914d916dc4fae42920539
IsaacSemb <isaacsemb1996@gmail.com>
2025-04-27 18:50:40 +0100

    Add toggler function to evaluate whether to show full description or not

--- vue-101/src/components/JobListing.vue
+++ vue-101/src/components/JobListing.vue
@@ -12,9 +12,9 @@ const fullDescription = ref(false)
 
 const truncateDescription = computed(
 
-    ()=>{
+    () => {
         let description = props.job.description
-        return fullDescription.value ? description : description.substring(0,50)+"..."
+        return fullDescription.value ? description : description.substring(0, 50) + "..."
     }
 
     // ()=>{
@@ -25,10 +25,11 @@ const truncateDescription = computed(
     //     }
     //     return description
     // }
-    
-
 )
 
+function toggleFullDescription(){
+    fullDescription.value = (!fullDescription.value)
+}
 
 
 </script>
@@ -40,16 +41,23 @@ const truncateDescription = computed(
     <div class="bg-white rounded-xl shadow-md relative">
         <div class="p-4">
             <div class="mb-6">
-                <div class="text-gray-600 my-2">{{job.type}}</div>
-                <h3 class="text-xl font-bold">{{job.title}}</h3>
+                <div class="text-gray-600 my-2">{{ job.type }}</div>
+                <h3 class="text-xl font-bold">{{ job.title }}</h3>
             </div>
 
             <div class="mb-5">
-                {{ truncateDescription }}
-                <!-- {{ job.description }} -->
+
+                <div>
+                    {{ truncateDescription }}
+                </div>
+
+                <button @click="toggleFullDescription" class="text-green-500 hover:text-green-600 mb-5">
+                    {{ fullDescription?'show less':'show more' }}
+                </button>
+            
             </div>
 
-            <h3 class="text-green-500 mb-2">{{job.salary}}</h3>
+            <h3 class="text-green-500 mb-2">{{ job.salary }}</h3>
 
             <div class="border border-gray-100 mb-5"></div>
 
@@ -58,7 +66,7 @@ const truncateDescription = computed(
                     <i class="fa-solid fa-location-dot text-lg"></i>
                     {{ job.location }}
                 </div>
-                <a :href="'/job/'+ job.id"
+                <a :href="'/job/' + job.id"
                     class="h-[36px] bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg text-center text-sm">
                     Read More
                 </a>
