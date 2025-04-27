fbb9826c23183016101f1cabd884f16b5896988e
IsaacSemb <isaacsemb1996@gmail.com>
2025-04-27 19:27:09 +0100

    Resolve imports for the components with the @ sign

--- vue-101/src/views/HomeView.vue
+++ vue-101/src/views/HomeView.vue
@@ -1,8 +1,8 @@
 
 <script setup>
-import Hero from './components/Hero.vue';
-import HomeCards from './components/HomeCards.vue';
-import JobListings from './components/JobListings.vue';
+import Hero from '@/components/Hero.vue';
+import HomeCards from '@/components/HomeCards.vue';
+import JobListings from '@/components/JobListings.vue';
 </script>
 
 <template>
