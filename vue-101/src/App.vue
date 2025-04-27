911911e057a419432de0f076d79fd232215adf58
IsaacSemb <isaacsemb1996@gmail.com>
2025-04-27 19:19:15 +0100

    creating the HomeView  and refactor it out of app view

--- vue-101/src/App.vue
+++ vue-101/src/App.vue
@@ -2,22 +2,12 @@
 <!-- scripting for logic -->
 <script setup>
 import NavBar from './components/NavBar.vue';
-import Hero from './components/Hero.vue';
-import HomeCards from './components/HomeCards.vue';
-import JobListings from './components/JobListings.vue';
 
 </script>
 
 <!-- html  -->
 <template>
     <NavBar />
-    <Hero 
-    title="This is Vue Jobs" 
-    subtitle="Hone and sharpen your skills"
-    />
-    <HomeCards />
-    <JobListings :limit="3" :view-all-jobs="true" />
-
 </template>
 
 <!-- css styling -->
---
+++ vue-101/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+
+<script setup>
+import Hero from './components/Hero.vue';
+import HomeCards from './components/HomeCards.vue';
+import JobListings from './components/JobListings.vue';
+</script>
+
+<template>
+    <Hero 
+    title="This is Vue Jobs" 
+    subtitle="Hone and sharpen your skills"
+    />
+    <HomeCards />
+    <JobListings :limit="3" :view-all-jobs="true" />
+</template>
