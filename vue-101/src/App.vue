e1c7ff6ea2ffdec71bb8c9db840423e449d534a0
IsaacSemb <isaacsemb1996@gmail.com>
2025-04-27 19:27:57 +0100

    Add router vue in main layout

--- vue-101/src/App.vue
+++ vue-101/src/App.vue
@@ -1,13 +1,16 @@
 
 <!-- scripting for logic -->
 <script setup>
+import { RouterView } from 'vue-router';
 import NavBar from './components/NavBar.vue';
+
 
 </script>
 
 <!-- html  -->
 <template>
     <NavBar />
+    <RouterView />
 </template>
 
 <!-- css styling -->
