c961af5e6f80327e145b50ee3dad05b6f04cb2e1
IsaacSemb <isaacsemb1996@gmail.com>
2025-04-27 20:58:47 +0100

    Adding the not found page

--- vue-101/src/router/index.js
+++ vue-101/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import JobsView from "@/views/JobsView.vue";
+import NotFoundView from "@/views/NotFoundView.vue";
 
 // create router takes an object
 const router = createRouter({
@@ -18,6 +19,11 @@ const router = createRouter({
       name: "jobs",
       component: JobsView,
     },
+    {
+      path:"/:catchAll(.*)",
+      name:"not-found",
+      component:NotFoundView
+    }
   ],
 });
 
