8daee3f10e27eda87d1c040c63456b6beeb3030f
IsaacSemb <isaacsemb1996@gmail.com>
2025-04-28 02:22:17 +0100

    Implement deletion of a job

--- vue-101/src/views/JobView.vue
+++ vue-101/src/views/JobView.vue
@@ -3,11 +3,15 @@
 import BackButton from '@/components/BackButton.vue';
 import axios from 'axios';
 import { onMounted, reactive } from 'vue';
-import { RouterLink, useRoute } from 'vue-router';
+import { RouterLink, useRoute, useRouter } from 'vue-router';
 import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
+import { useToast } from 'vue-toastification';
 
 // route for getting the current route
 const route = useRoute()
+
+const router = useRouter()
+const toast = useToast()
 
 // get id from route
 const jobId = route.params.id
@@ -38,21 +42,39 @@ onMounted(
 )
 
 
+const handleDeleteJobClick = async () => {
+  try {
+    const confirm = window.confirm("Are sure you want to delete this job?")
+
+    if (confirm) {
+      await axios.delete(`/api/jobs/${jobId}`)
+      toast.success("Job deleted sucessfully")
+      router.push('/jobs')
+    }
+
+  } catch (error) {
+    console.error(error);
+    toast.error("Job Not deleted")
+  }
+
+}
+
+
 
 
 </script>
 
 
 <template>
-  
+
   <BackButton />
-  
+
   <!-- show loading spinner while loading is true -->
   <div v-if="jobState.isLoading" class=" text-center text-gray-500 py-6">
     <PulseLoader />
   </div>
 
-  
+
 
 
   <section v-else class="bg-green-50">
@@ -74,7 +96,7 @@ onMounted(
             </h3>
 
             <p class="mb-4">
-              {{jobState.job.description}}
+              {{ jobState.job.description }}
             </p>
 
             <h3 class="text-green-800 text-lg font-bold mb-2">Salary</h3>
@@ -109,11 +131,11 @@ onMounted(
           <!-- Manage -->
           <div class="bg-white p-6 rounded-lg shadow-md mt-6">
             <h3 class="text-xl font-bold mb-6">Manage Job</h3>
-            <RouterLink 
-              :to= "`/jobs/edit/${jobId}`"
-              class="bg-green-500 hover:bg-green-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">Edit
+            <RouterLink :to="`/jobs/edit/${jobId}`"
+              class="bg-green-500 hover:bg-green-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
+              Edit
               Job</RouterLink>
-            <button
+            <button @click="handleDeleteJobClick"
               class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
               Delete Job
             </button>
